8bcc923145876c20d4bc84b4d4836371d4b1b003
yahooserg <seltexinc@gmail.com>
2021-11-25 11:04:07 +0700

    Sushi start

---
+++ fvsrv4s.js
@@ -0,0 +1,37 @@
+/*jslint nomen: true, node: true, unparam: true*/
+(function () {
+    "use strict";
+    var createRevenueData = require('./services/createrevenuedata4s.js'),
+      fs = require('fs'),
+      myFunctions = require('./services/myfunctions'),
+      revenueStore = function () {
+        var date = new Date();
+        var hour = date.getUTCHours()+3,
+            minute = date.getUTCMinutes();
+        // if(hour <= 23 && hour >= 8 && minute % 15 === 8) {
+          console.log("Start: ", date);
+          // createRevenueData.getDataFromDB(function (data) {
+          //   date = new Date();
+          //   var currentDate = myFunctions.getDateString(date) + " " + myFunctions.getTimeString(date);
+          //   data[data.length] = currentDate;
+          //   fs.writeFile(__dirname + "/../kvdata.json", JSON.stringify(data), function (err) {
+          //     if(err) {
+          //       console.log(err);
+          //     }
+          //     date = new Date();
+          //     console.log("Finish: ", date);
+          //   })
+          // });
+          createRevenueData.getStores(function (data) {
+            fs.writeFile(__dirname + "/../stores4s.json", JSON.stringify(data), function (err) {
+              if(err) {
+                console.log(err);
+              }
+            })
+          });
+        // }
+
+      };
+    // revenueStore();
+    setInterval(revenueStore, 60000);
+}());
